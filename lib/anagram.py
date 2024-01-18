5f59b54f6e321126ca0ef68ea8ec03af20c0e8aa
maurice diong <mdiong00@gmail.com>
2024-01-18 11:31:44 -0500

    I could do it in less line sbut for another time. remember that self is an object

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+
+    def match(self,words):
+        result=[]
+        # breakpoint()
+        for word_match in words:
+            if sorted([letter for letter in word_match]) == sorted([letter for letter in self.word]):
+                result.append(word_match)
+        return result
